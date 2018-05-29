01148808e1b34e2920e9e6f1181c775be3f387fb
lichao <jiexuelc@outlook.com>
2018-05-29 22:29:18 +0800

    template

---
+++ temp.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: temp.cpp
+	> Author: jiexue
+	> Mail: jiexuelc@163.com
+	> Created Time: Tue 29 May 2018 10:28:06 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
