e805bfd1ba61262f05a4b552b8892bcf75120624
lichao <jiexuelc@outlook.com>
2018-06-09 22:12:03 +0800

    输出一个指定区间中的所有数据（含头不含尾）

---
+++ Template/STL/print.h
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: print.h
+	> Author: jiexue
+	> Mail: jiexuelc@163.com
+	> Created Time: Sat 09 Jun 2018 09:46:30 PM CST
+	> Description:输出一个指定区间中的所有数据（含头不含尾）
+ ************************************************************************/
+
+#ifndef _PRINT_H
+#define _PRINT_H 1
+using namespace std;
+template <typename T>
+void print(T beg, T end, char ch = ' ')
+{
+	while(beg != end){
+		cout << *beg++ << ch;
+	}
+	cout << endl;
+}
+#endif
